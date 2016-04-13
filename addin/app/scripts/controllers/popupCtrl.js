c1833a7c2ce8f77916398c8c511afadc06d87c60
Lozach, Pierrick <Pierrick.lozach@inin.com>
2016-04-13 19:12:16 +0200

    Logger provider additions

--- addin/app/scripts/controllers/popupCtrl.js
+++ addin/app/scripts/controllers/popupCtrl.js
@@ -12,6 +12,12 @@ angular.module('app', [])
   .controller('popupCtrl', function($rootScope, $scope, $log) {
     
     $scope.testLog = function(message) {
+      console.log('message from testLog');
+      try {
+        $log.isEnabled;
+      } catch (error) {
+        console.log(error);
+      }
       $log.debug(message);
     }
   });
