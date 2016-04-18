079f4762e7cb580ea1ec6789d73fbcc0c8f221a8
Dariusz_Socha <sodax@Dariuszs-MacBook-Pro.local>
2016-04-18 17:16:53 +0200

    CIC options

--- addin/app/scripts/controllers/optionsCtrl.js
+++ addin/app/scripts/controllers/optionsCtrl.js
@@ -9,42 +9,77 @@
 **/
 
 angular.module('app', ['chromeStorage'])
-    .controller('optionsCtrl', function ($rootScope, $scope, $log, chromeStorage) {
-
-        // <Initializing>      
-        getPcOptions();
-        // </Initializing>
-
-        // <PureCloud options>
-        $scope.pcOptionsChanged = function () {
-            try {
-                var pcOptions = {};
-                pcOptions.pcAuthUrl = $scope.pcAuthUrl;
-                pcOptions.pcAccessTokenUrl = $scope.pcAccessTokenUrl;
-                pcOptions.pcClientId = $scope.pcClientId;
-                pcOptions.pcClientSecret = $scope.pcClientSecret;
-                var storageItem = {};
-                storageItem['pcOptions'] = pcOptions;
-                chrome.storage.local.set(storageItem);
-            }
-            catch (err) {
-                $log.error(err);
-            }
-        };
-
-        function getPcOptions() {
-            try {
-                chromeStorage.get('pcOptions').then(function (pcOptions) {
-                    $scope.pcAuthUrl = pcOptions.pcAuthUrl;
-                    $scope.pcAccessTokenUrl = pcOptions.pcAccessTokenUrl;
-                    $scope.pcClientId = pcOptions.pcClientId;
-                    $scope.pcClientSecret = pcOptions.pcClientSecret;
-                });
-            }
-            catch (err) {
-                $log.error(err);
-            }
-        }
-        // </PureCloud options>
-
-    });
+  .controller('optionsCtrl',function($rootScope, $scope, $log, chromeStorage) {
+        
+    // <Initializing>      
+    getPcOptions();
+    getIcOptions();
+    // </Initializing>
+    
+    // <PureCloud options>
+    $scope.pcOptionsChanged = function(){                                
+        try {
+            var pcOptions = {}; 
+            pcOptions.pcAuthUrl = $scope.pcAuthUrl;                
+            pcOptions.pcAccessTokenUrl = $scope.pcAccessTokenUrl;
+            pcOptions.pcClientId = $scope.pcClientId;
+            pcOptions.pcClientSecret = $scope.pcClientSecret;            
+            var storageItem = {};
+            storageItem['pcOptions'] = pcOptions;                 
+            chrome.storage.local.set(storageItem);
+        } 
+        catch (err) {
+            $log.err(err);
+        }                
+    };
+        
+    function getPcOptions() {
+        try {                       
+            chromeStorage.get('pcOptions').then(function(pcOptions) {
+                $scope.pcAuthUrl = pcOptions.pcAuthUrl;
+                $scope.pcAccessTokenUrl = pcOptions.pcAccessTokenUrl;
+                $scope.pcClientId = pcOptions.pcClientId;
+                $scope.pcClientSecret = pcOptions.pcClientSecret;
+            });
+        }  
+        catch (err) {
+            $log.err(err);
+        }         
+    }
+    // </PureCloud options>
+    
+    // <CIC options>
+    $scope.icOptionsChanged = function(){                                
+        try {
+            var icOptions = {}; 
+            icOptions.icIcServer = $scope.icIcServer;                
+            icOptions.icPort = $scope.icPort;            
+            icOptions.icUsername = $scope.icUsername;
+            icOptions.icPassword = $scope.icPassword;  
+            icOptions.icUseSsl = $scope.icUseSsl;          
+            var storageItem = {};
+            storageItem['icOptions'] = icOptions;                 
+            chrome.storage.local.set(storageItem);
+        } 
+        catch (err) {
+            $log.err(err);
+        }                
+    };
+        
+    function getIcOptions() {
+        try {                       
+            chromeStorage.get('icOptions').then(function(icOptions) {
+                $scope.icIcServer = icOptions.icIcServer;
+                $scope.icPort = icOptions.icPort;                
+                $scope.icUsername = icOptions.icUsername;
+                $scope.icPassword = icOptions.icPassword;
+                $scope.icUseSsl = icOptions.icUseSsl;
+            });
+        }  
+        catch (err) {
+            $log.err(err);
+        }         
+    }
+    // </CIC options>
+    
+  });
