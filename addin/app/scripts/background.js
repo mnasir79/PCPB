26bd930b450e0b539d4e280b1b867ec32977ab10
Sebastian Pusz <sebastian.pusz@inin.com>
2016-04-20 15:49:54 +0200

    sample communication between popup and backgroundCtrl

--- addin/app/scripts/background.js
+++ addin/app/scripts/background.js
@@ -7,6 +7,6 @@ chrome.runtime.onInstalled.addListener(function (details) {
 console.log('Chrome Extension Id: ' + chrome.runtime.id);
 
 function getController() {
-    var scope = angular.element(document.getElementById("backgroundCtrl")).scope();
+    var scope = angular.element(document.getElementById('backgroundCtrl')).scope();
     return scope;
 }
