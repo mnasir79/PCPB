10206c57c9ea2a52d392a6b2ed26f4977be406ae
Sebastian Schnack <sebastian.schnack@inin.com>
2016-04-22 09:36:53 +0200

    No Changes, necessary update to Pull newest Version

--- addin/app/scripts/background.js
+++ addin/app/scripts/background.js
@@ -4,5 +4,4 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-console.log('Chrome Extension Id: ' + chrome.runtime.id);
 console.log('\'Allo \'Allo! Event Page for Browser Action');
