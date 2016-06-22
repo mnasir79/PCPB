05c1270a0b357268de94c5d56cfb99a149ac0ae0
Dariusz_Socha <sodax@Dariuszs-MacBook-Pro.local>
2016-06-22 15:49:17 +0200

    translating CIC JSON to PBI JSON

--- addin/app/scripts/services/jsonTranslator.js
+++ addin/app/scripts/services/jsonTranslator.js
@@ -4,15 +4,43 @@
 angular.module('jsonTranslator', ['ngJSONPath'])
     .service('jsonTranslator', function (jsonPath) {
 
-        this.translateCicStatSet = function(input){
-            var statRootPath = 'data.[0].statisticValueChanges';
-            var statNamePath = 'statisticKey.statisticIdentifier';
-            var workgroupPath = 'statisticKey.parameterValueItems.value';            
-            var valuePath = 'statisticValue.value';
+        this.translateCicStatSet = function (input) {
+            // initializing json paths
+            var statRootPath = '$.[0].statisticValueChanges'; //absolute
+            var statNamePath = 'statisticKey.statisticIdentifier'; //relative
+            var workgroupPath = 'statisticKey.parameterValueItems[0].value'; //relative           
+            var valuePath = 'statisticValue.value'; //relative
 
-            var statRoot = jsonPath(input, statRootPath);
+            // getting an array with statistics
+            var statRoot = jsonPath(input, statRootPath)[0];
             
-            console.debug(input);
-            console.debug(statRoot);
-        }               
+            // creating an output object
+            var output = { "data" : [] };
+            
+            // iterating all statistics in the array
+            for (var i in statRoot) {
+                var workgroup = jsonPath(statRoot[i], workgroupPath)[0];
+                var statName = jsonPath(statRoot[i], statNamePath)[0].replace('inin.workgroup:','');                                              
+                var value = jsonPath(statRoot[i], valuePath)[0];
+                var item = jsonPath(output, "$.data.[?(@.name==='" + workgroup + "')]");
+                if (item) {
+                    // <updating the existing workgroup>                    
+                    for (var j in output.data){                         
+                        if (output.data[j].name == workgroup){
+                            output.data[j][statName] = value;
+                        }
+                    }   
+                    // </updating the existing workgroup>                 
+                }
+                else {
+                    // <creating a new workgroup>
+                    var newItem = new Object();
+                    newItem['name'] = workgroup;
+                    newItem[statName] = value;
+                    output.data.push(newItem);
+                    // </creating a new workgroup>
+                }
+            }
+            return output;
+        }
     });
